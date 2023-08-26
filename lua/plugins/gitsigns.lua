9e43f3bf69ec961f71629c9f858917ae2b3c00e3
Simon H Moore <simon@simonhugh.xyz>
2023-08-26 20:27:31 +0100

    Add: gitsigns plugin

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,23 @@
+-- gitsigns.lua
+
+
+return {
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    -- See `:help gitsigns.txt`
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+    on_attach = function(bufnr)
+      vim.keymap.set('n', '[h', require('gitsigns').prev_hunk,
+        { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+      vim.keymap.set('n', ']h', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+      vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+    end,
+  },
+}
