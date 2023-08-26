a7444609859dae36825dac6a3a8ab772bd73ad51
Simon H Moore <simon@simonhugh.xyz>
2023-08-26 20:15:43 +0100

    Add: leader key definitions

--- lua/init.lua
+++ lua/init.lua
@@ -1,4 +1,8 @@
 -- init.lua
 
+-- Leader keys need to be set first
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '  '
+
 require('options') -- Import basic neovim options
 require('lazy_setup') -- Install lazy plugin manager and set up plugins
