1606e1001e7c7c2d04934f067f10861e63eb0177
Simon H Moore <simon@simonhugh.xyz>
2023-08-26 20:03:56 +0100

    Add: gruvbox colorscheme plugin

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,26 @@
+-- gruvbox.lua
+--
+-- A colorscheme
+
+
+local o = vim.opt
+local g = vim.g
+local cmd = vim.cmd
+
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      require("gruvbox").setup({
+        transparent_mode = true,
+      })
+      o.background = "dark"
+      g.gruvbox_italic = true
+      g.gruvbox_bold = false
+      g.gruvbox_transparent_bg = true
+      g.gruvbox_constrast_dark = "hard"
+      g.gruvbox_improved_strings = false
+      cmd([[colorscheme gruvbox]])
+    end,
+  },
+}
