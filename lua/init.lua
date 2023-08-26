e8478e558547f466652676f5a70f161de18407d3
Simon H Moore <simon@simonhugh.xyz>
2023-08-26 20:21:04 +0100

    Add: autocommands

--- lua/init.lua
+++ lua/init.lua
@@ -8,3 +8,4 @@ require('options') -- Import basic neovim options
 require('lazy_setup') -- Install lazy plugin manager and set up plugins
 require('keymaps') -- Set up non plugin specific keymaps
 require('highlights') -- Set up highlights
+require('autocommands') -- Set up autocommands
