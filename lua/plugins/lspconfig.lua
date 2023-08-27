db60fe83b0b3bbe157e75c112343c0dd8904d820
Simon H Moore <simon@simonhugh.xyz>
2023-08-27 17:25:23 +0100

    Add: lspconfig and related plugins
    - nvim-lspconfig
    - mason
    - mason-lspconfig
    - fidget
    - neodev

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,19 @@
+-- lspconfig.lua
+--
+-- language server config plugin
+
+return {
+  -- LSP Configuration & Plugins
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    -- Automatically install LSPs to stdpath for neovim
+    { 'williamboman/mason.nvim', config = true },
+    'williamboman/mason-lspconfig.nvim',
+
+    -- Useful status updates for LSP
+    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+
+    -- Additional lua configuration, makes nvim stuff amazing!
+    { "folke/neodev.nvim", opts = {} }
+  },
+}
