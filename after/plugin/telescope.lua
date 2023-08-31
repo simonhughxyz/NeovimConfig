2c787a7f654d800cbebe5504dfb6328ef85f034c
Simon H Moore <simon@simonhugh.xyz>
2023-08-31 21:49:39 +0100

    Refactor: move telescope keymap config to after/plugin

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,32 @@
+-- telescope.lua
+
+
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
+
+local ts = require('telescope.builtin')
+
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', ts.oldfiles, { desc = 'Find recently opened files' })
+vim.keymap.set('n', '<leader>,', ts.buffers, { desc = 'Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  ts.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = 'Fuzzily search in current buffer' })
+
+vim.keymap.set('n', '<leader>gf', ts.git_files, { desc = 'Search Git Files' })
+vim.keymap.set('n', '<leader>ff', ts.find_files, { desc = 'Search Files' })
+vim.keymap.set('n', '<leader>sh', ts.help_tags, { desc = 'Search Help' })
+vim.keymap.set('n', '<leader>sw', ts.grep_string, { desc = 'Search current Word' })
+vim.keymap.set('n', '<leader>sg', ts.live_grep, { desc = 'Search by Grep' })
+vim.keymap.set('n', '<leader>sd', ts.diagnostics, { desc = 'Search Diagnostics' })
+vim.keymap.set('n', '<leader>sk', ts.keymaps, { desc = 'Search Keymaps' })
+
+
+vim.keymap.set('n', '<leader>fd', function() ts.find_files({ cwd = '~/Documents' }) end, { desc = 'Find Documents' })
+vim.keymap.set('n', '<leader>fp', function() ts.find_files({ cwd = '~/Projects' }) end, { desc = 'Find Projects' })
+vim.keymap.set('n', '<leader>fc', function() ts.find_files({ cwd = '~/.config' }) end, { desc = 'Find Config' })
+vim.keymap.set('n', '<leader>fb', function() ts.find_files({ cwd = '~/.local/bin' }) end, { desc = 'Find Local Bin' })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,42 +19,4 @@ return {
       end,
     },
   },
-  opts = function()
-    -- Enable telescope fzf native, if installed
-    pcall(require('telescope').load_extension, 'fzf')
-
-    -- See `:help telescope.builtin`
-    vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-    vim.keymap.set('n', '<leader>,', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to telescope to change theme, layout, etc.
-      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end, { desc = '[/] Fuzzily search in current buffer' })
-
-    vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-  end,
-  keys = {
-    -- change a keymap
-    { "<leader>fd", "<cmd>Telescope find_files cwd=~/Documents<cr>",  desc = "Find Documents" },
-    { "<leader>fp", "<cmd>Telescope find_files cwd=~/Projects<cr>",   desc = "Find Projects" },
-    { "<leader>fc", "<cmd>Telescope find_files cwd=~/.config<cr>",    desc = "Find Config" },
-    { "<leader>fb", "<cmd>Telescope find_files cwd=~/.local/bin<cr>", desc = "[?] Find recently opened files" },
-    { "<leader>f?", "<cmd>Telescope oldfiles<cr>",                    desc = "Find Local Bin" },
-    -- add a keymap to browse plugin files
-    {
-      "<leader>fP",
-      function()
-        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-      end,
-      desc = "Find Plugin File",
-    },
-  },
 }
