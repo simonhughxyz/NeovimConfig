cd004f24754d592aafe40073e3b17931728033cd
Simon H Moore <simon@simonhugh.xyz>
2023-09-08 20:47:39 +0100

    Refactor: indent-blankline plugin
    - indent-blankline plugin gets its own file
    - add excluded filetypes
    - show current context

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -12,18 +12,6 @@ return {
   { 'monaqa/dial.nvim' },
 
   {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    enabled = true,
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
-
-  {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
