045c52cc8d99521e23a9e63a1d930b4e50c667d8
Simon H Moore <simon@simonhugh.xyz>
2023-09-14 22:30:47 +0100

    Refactor: align comments

--- lua/init.lua
+++ lua/init.lua
@@ -4,8 +4,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '  '
 
-require('options') -- Import basic neovim options
-require('lazy_setup') -- Install lazy plugin manager and set up plugins
-require('keymaps') -- Set up non plugin specific keymaps
-require('highlights') -- Set up highlights
+require('options')      -- Import basic neovim options
+require('lazy_setup')   -- Install lazy plugin manager and set up plugins
+require('keymaps')      -- Set up non plugin specific keymaps
+require('highlights')   -- Set up highlights
 require('autocommands') -- Set up autocommands
