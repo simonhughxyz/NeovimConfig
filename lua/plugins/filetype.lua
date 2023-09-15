e85d155d406b69a6e144bcc29cc8f82b04c2f127
Simon H Moore <simon@simonhugh.xyz>
2023-09-16 00:09:35 +0100

    Refactor: format code

--- lua/plugins/filetype.lua
+++ lua/plugins/filetype.lua
@@ -3,14 +3,14 @@
 -- Fileype specific plugins
 
 return {
-  { "chrisbra/csv.vim", ft = { "csv" } },
-  { "ledger/vim-ledger", ft = { "ledger" } },
-  { "tmux-plugins/vim-tmux", ft = { "tmux" } },
+  { "chrisbra/csv.vim",           ft = { "csv" } },
+  { "ledger/vim-ledger",          ft = { "ledger" } },
+  { "tmux-plugins/vim-tmux",      ft = { "tmux" } },
   -- { "baskerville/vim-sxhkdrc", ft = { "sxhkdrc" } },
   { "fourjay/vim-password-store", ft = { "pass" } },
-  { "tmhedberg/SimpylFold", ft = { "python" } },
-  { "tridactyl/vim-tridactyl", ft = { "trydactyl" } },
-  { "neomutt/neomutt.vim", ft = { "muttrc" } },
-  { "pangloss/vim-javascript", ft = { "javascript" } },
+  { "tmhedberg/SimpylFold",       ft = { "python" } },
+  { "tridactyl/vim-tridactyl",    ft = { "trydactyl" } },
+  { "neomutt/neomutt.vim",        ft = { "muttrc" } },
+  { "pangloss/vim-javascript",    ft = { "javascript" } },
   { "leafgarland/typescript-vim", ft = { "typescript" } },
 }
