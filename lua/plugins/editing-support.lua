52fe4305104475a832af435095df0d8a6d196e4f
Simon H Moore <simon@simonhugh.xyz>
2023-10-01 21:31:36 +0100

    Refactor: move plugins to editing-support.lua
    - Comment.nvim
    - dial.nvim
    - nvim-various-textobjs
    - treesj
    - text-case.nvim
    
    moved to editing-support.lua

---
+++ lua/plugins/editing-support.lua
@@ -0,0 +1,68 @@
+-- editing-support.lua
+--
+-- Plugins that help with editing text and files
+
+return {
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  -- Extended increment/decrement plugin
+  { 'monaqa/dial.nvim' },
+
+  { -- Bundle of more than two dozen new textobjects for Neovim.
+    "chrisgrieser/nvim-various-textobjs",
+    lazy = false,
+    opts = { useDefaultKeymaps = true },
+  },
+
+  { -- split-join lines
+    "Wansmer/treesj",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    keys = {
+      { "<leader>j", function() require("treesj").toggle() end, desc = "󰗈 Split-join lines" },
+    },
+  },
+
+  { -- case conversion, upper to lower to camel to snake...
+    "johmsalas/text-case.nvim",
+    init = function()
+      local casings = {
+        { char = "u", arg = "upper",      desc = "UPPER CASE" },
+        { char = "l", arg = "lower",      desc = "lower case" },
+        { char = "t", arg = "title",      desc = "Title Case" },
+        { char = "c", arg = "camel",      desc = "camelCase" },
+        { char = "C", arg = "pascal",     desc = "CamelCase" },
+        { char = "s", arg = "snake",      desc = "snake_case" },
+        { char = "_", arg = "snake",      desc = "snake_case" },
+        { char = "d", arg = "dash",       desc = "dash-case" },
+        { char = "D", arg = "title_dash", desc = "Title-Dash-Case" },
+        { char = "-", arg = "dash",       desc = "dash-case" },
+        { char = "p", arg = "phrase",     desc = "Phrase case" },
+        { char = "/", arg = "path",       desc = "path/case" },
+        { char = "S", arg = "constant",   desc = "UPPER_SNAKE_CASE" },
+        { char = ".", arg = "dot",        desc = "dot.case" },
+      }
+
+      for _, case in pairs(casings) do
+        vim.keymap.set(
+          "n",
+          "<leader>t" .. case.char,
+          ("<cmd>lua require('textcase').current_word('to_%s_case')<CR>"):format(case.arg),
+          { desc = case.desc }
+        )
+        vim.keymap.set(
+          "v",
+          "<leader>t" .. case.char,
+          ("<cmd>lua require('textcase').operator('to_%s_case')<CR>"):format(case.arg),
+          { desc = case.desc }
+        )
+        vim.keymap.set(
+          "n",
+          "<leader>T" .. case.char,
+          ("<cmd>lua require('textcase').lsp_rename('to_%s_case')<CR>"):format(case.arg),
+          { desc = "󰒕 " .. case.desc }
+        )
+      end
+    end,
+  },
+}
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -4,12 +4,7 @@
 
 return {
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  opts = {} },
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
-  { 'monaqa/dial.nvim' },
+  { 'folke/which-key.nvim', opts = {} },
 
   {
     "folke/trouble.nvim",
@@ -26,63 +21,5 @@ return {
     opts = {},
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
-
-  { -- Bundle of more than two dozen new textobjects for Neovim.
-    "chrisgrieser/nvim-various-textobjs",
-    lazy = false,
-    opts = { useDefaultKeymaps = true },
-  },
-
-  { -- split-join lines
-    "Wansmer/treesj",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    keys = {
-      { "<leader>j", function() require("treesj").toggle() end, desc = "󰗈 Split-join lines" },
-    },
-  },
-  {   -- case conversion
-    { -- converts a piece of text to an indicated string case
-      "johmsalas/text-case.nvim",
-      init = function()
-        local casings = {
-          { char = "u", arg = "upper",      desc = "UPPER CASE" },
-          { char = "l", arg = "lower",      desc = "lower case" },
-          { char = "t", arg = "title",      desc = "Title Case" },
-          { char = "c", arg = "camel",      desc = "camelCase" },
-          { char = "C", arg = "pascal",     desc = "CamelCase" },
-          { char = "s", arg = "snake",      desc = "snake_case" },
-          { char = "_", arg = "snake",      desc = "snake_case" },
-          { char = "d", arg = "dash",       desc = "dash-case" },
-          { char = "D", arg = "title_dash", desc = "Title-Dash-Case" },
-          { char = "-", arg = "dash",       desc = "dash-case" },
-          { char = "p", arg = "phrase",     desc = "Phrase case" },
-          { char = "/", arg = "path",       desc = "path/case" },
-          { char = "S", arg = "constant",   desc = "UPPER_SNAKE_CASE" },
-          { char = ".", arg = "dot",        desc = "dot.case" },
-        }
-
-        for _, case in pairs(casings) do
-          vim.keymap.set(
-            "n",
-            "<leader>t" .. case.char,
-            ("<cmd>lua require('textcase').current_word('to_%s_case')<CR>"):format(case.arg),
-            { desc = case.desc }
-          )
-          vim.keymap.set(
-            "v",
-            "<leader>t" .. case.char,
-            ("<cmd>lua require('textcase').operator('to_%s_case')<CR>"):format(case.arg),
-            { desc = case.desc }
-          )
-          vim.keymap.set(
-            "n",
-            "<leader>T" .. case.char,
-            ("<cmd>lua require('textcase').lsp_rename('to_%s_case')<CR>"):format(case.arg),
-            { desc = "󰒕 " .. case.desc }
-          )
-        end
-      end,
-    },
   },
 }
