b2937c0bdafea2df4ad755dbd7f0c938375182c7
Simon H Moore <simon@simonhugh.xyz>
2023-10-16 21:44:46 +0100

    Refactor: move spider config to lazy

--- after/plugin/spider.lua
+++
@@ -1,6 +0,0 @@
--- spider.lua
-
-vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<cr>", { desc = "Spider-w" })
-vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<cr>", { desc = "Spider-e" })
-vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<cr>", { desc = "Spider-b" })
-vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<cr>", { desc = "Spider-ge" })
--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -7,6 +7,12 @@ return {
     "chrisgrieser/nvim-spider",
     enabled = true,
     lazy = true,
+    config = function()
+      vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<cr>", { desc = "Spider-w" })
+      vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<cr>", { desc = "Spider-e" })
+      vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<cr>", { desc = "Spider-b" })
+      vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<cr>", { desc = "Spider-ge" })
+    end,
   },
 
   { -- to highlight and search for todo comments like TODO, HACK, BUG in your code base.
