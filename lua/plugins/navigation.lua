fcc49d975db2a727f643989fbb961301d13b5299
Simon H Moore <simon@simonhugh.xyz>
2023-10-16 19:57:48 +0100

    Refactor[navigation.lua] move harpoon key maps to `<leader>'`

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -88,9 +88,9 @@ return {
       require("telescope").load_extension('harpoon')
 
       -- set keymaps
-      vim.keymap.set('n', '<leader>Ha', require('harpoon.mark').add_file, { desc = 'Harpoon Add File' })
-      vim.keymap.set('n', '<leader>H]', require('harpoon.ui').nav_next, { desc = 'Harpoon Next' })
-      vim.keymap.set('n', '<leader>H[', require('harpoon.ui').nav_prev, { desc = 'Harpoon Previous' })
+      vim.keymap.set('n', "<leader>'a", require('harpoon.mark').add_file, { desc = 'Harpoon Add File' })
+      vim.keymap.set('n', "<leader>']", require('harpoon.ui').nav_next, { desc = 'Harpoon Next' })
+      vim.keymap.set('n', "<leader>'[", require('harpoon.ui').nav_prev, { desc = 'Harpoon Previous' })
       vim.keymap.set("n", "<leader>fh", ':Telescope harpoon marks<cr>', { desc = "Find Harpoon" })
     end,
   },
