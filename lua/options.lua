e849e86e73d50287e0a3ebb652456182bef0b7b7
Simon H Moore <simon@simonhugh.xyz>
2023-10-19 06:48:52 +0100

    Add: set default shell to powershell on windows

--- lua/options.lua
+++ lua/options.lua
@@ -50,3 +50,14 @@ if vim.fn.has("nvim-0.9.0") == 1 then
   o.splitkeep = "screen"
   o.shortmess:append({ C = true })
 end
+
+-- use powershell on windows
+if vim.fn.has("win32") == 1 then
+  o.shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell"
+  o.shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  o.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+  o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit LastExitCode"
+  o.shellquote = ""
+  o.shellxquote = ""
+end
