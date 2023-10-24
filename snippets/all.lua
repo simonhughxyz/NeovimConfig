b64c4f035f757d5af042e1e888b583717a7f8d8e
Simon H Moore <simon@simonhugh.xyz>
2023-10-24 22:26:36 +0100

    Add: date and time snippets

--- snippets/all.lua
+++ snippets/all.lua
@@ -29,4 +29,49 @@ return {
     }),
   }),
   s('wsign', { t("Simon H Moore <simonm@vigoitsolutions.com>") }),
+  s({
+      trig = 'date',
+      priority = 10000,
+      desc = 'Current date'
+    },
+    {
+      c(1, {
+        f(function() return os.date "%d %b %Y" end),
+        f(function() return os.date "%d %B %Y" end),
+        f(function() return os.date "%a %d %b %Y" end),
+        f(function() return os.date "%A %d %b %Y" end),
+        f(function() return os.date "%a %d %B %Y" end),
+        f(function() return os.date "%A %d %B %Y" end),
+        f(function() return os.date "%d-%m-%Y" end),
+        f(function() return os.date "%d/%m/%Y" end),
+        f(function() return os.date "%d-%m-%y" end),
+        f(function() return os.date "%d/%m/%y" end),
+      })
+    }),
+  s({
+      trig = 'americandate',
+      priority = 10000,
+      desc = 'Current american date, month comes first',
+    },
+    {
+      c(1, {
+        f(function() return os.date "%m/%d/%Y" end),
+        f(function() return os.date "%m-%d-%Y" end),
+        f(function() return os.date "%m/%d/%y" end),
+        f(function() return os.date "%m-%d-%y" end),
+      })
+    }),
+  s({
+      trig = 'time',
+      priority = 10000,
+      desc = 'Current time',
+    },
+    {
+      c(1, {
+        f(function() return os.date "%H:%M" end),
+        f(function() return os.date "%I:%M %p" end),
+        f(function() return os.date "%H:%M:%S" end),
+        f(function() return os.date "%I:%M:%S %p" end),
+      })
+    }),
 }
